bfb155165aa0979b8764348403a00477568a7f93
Manoranjan6 <manoranjangupta123456@gmail.com>
2026-04-12 17:52:06 +0530

    Add files via upload

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -6,21 +6,33 @@ const Admin = require("../models/Admin");
 
 // LOGIN
 router.post("/login", async (req, res) => {
-  const { username, password } = req.body;
+  try {
+    const { username, password } = req.body;
 
-  const admin = await Admin.findOne({ username, password });
+    console.log("Received:", username, password);
 
-  if (!admin) {
-    return res.status(401).json({ message: "Invalid credentials" });
+    // find admin
+    const admin = await Admin.findOne({ username, password });
+
+    console.log("Found admin:", admin);
+
+    if (!admin) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    // generate token
+    const token = jwt.sign(
+      { id: admin._id },
+      process.env.JWT_SECRET,
+      { expiresIn: "2h" }
+    );
+
+    res.json({ token });
+
+  } catch (err) {
+    console.error("Login Error:", err);
+    res.status(500).json({ message: "Server error" });
   }
-
-  const token = jwt.sign(
-    { id: admin._id },
-    process.env.JWT_SECRET,
-    { expiresIn: "2h" }
-  );
-
-  res.json({ token });
 });
 
 module.exports = router;
