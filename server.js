28e9a0c4fb5b5e8d16b8235d585467d5d56bc015
Manoranjan6 <manoranjangupta123456@gmail.com>
2026-04-20 18:05:27 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(express.json({ limit: "10mb" })); // 🔥 prevent payload abuse
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
-      dbName: "schoolDB"
+      dbName: "schooldb"
     });
 
     console.log("✅ MongoDB Connected");
